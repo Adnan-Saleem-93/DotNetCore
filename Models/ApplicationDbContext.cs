0d36a1ef99d487f8d5bb60bc1911c848ad3f4caa
Adnan Saleem <adnan13893@gmail.com>
2022-01-29 20:00:47 +0500

    Added Entity Framework and created Database and table using Migrations

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BooksRazorApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
