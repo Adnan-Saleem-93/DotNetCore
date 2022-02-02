4ee11c9ad669accbd1ac19fb21ed996bec6a62af
Adnan Saleem <adnan13893@gmail.com>
2022-02-02 17:54:37 +0500

    Added controller

---
+++ Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using BooksRazorApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksRazorApp.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Book Book { get; set; }
+        public IActionResult GetBooks()
+        {
+            return Json(new { data = _db.Book.Find() });
+        }
+    }
+}
